6c689fa49677342138fab113c0e03a40931f855b
github username <github email address>
2021-04-18 21:45:07 -0400

    displayed dog information

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    fetch(`http://localhost:3000/dogs`)
+    .then(res => res.json())
+    .then(data => displayDogs(data))
+    
+    function displayDogs(data){
+        for (let i = 0; data.length > i; i++){
+            const createTr = document.createElement('tr');
+            
+            createTr.innerHTML = `
+            <td>${data[i].name}</td>
+            <td>${data[i].breed}</td>
+            <td>${data[i].sex}</td>
+            <button id='edit-dog-button'>Edit</button>
+            `
+            document.getElementById('table-body').appendChild(createTr);
+        }
+    }
 })
